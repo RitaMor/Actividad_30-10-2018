d7ecc42dd995473f98ed67ea1c73c9da07024faa
Rita <Rita@LAPTOP-DKKKA8R3>
2018-10-30 12:51:19 +0000

    Actividad_30/10/2018

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -28,16 +28,4 @@ public class Ejercicios {
 		return (acumulado / n);
 	}
 
-	/*public static void main(String[] args) {
-		Ejercicios ejercicios = new Ejercicios();
-
-		// Ejercicio 1
-		float[] movimientos = { 200f, 53.2f, 30.5f, -86.5f, -22.6f };
-		float saldoInicial = 300f;
-		ejercicios.calcularSaldoFinal(saldoInicial, movimientos);
-
-		// ejercicio 2
-		int resultado = ejercicios.ejercicio2(10, 5, 15);
-		
-	*/
 }
